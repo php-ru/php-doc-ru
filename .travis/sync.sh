317e650754c3d279f7ffc4a3730c362d1a4b74cf
Alexey Pyltsyn <lex61rus@gmail.com>
2018-03-01 11:29:32 +0300

    Test CI

--- .travis/sync.sh
+++ .travis/sync.sh
@@ -5,11 +5,7 @@ if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
     exit 0
 fi
 
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-
 git remote set-url origin https://lex111:${GITHUB_TOKEN}@github.com/lex111/php-doc-ru
-
+git update-ref refs/remotes/git-svn refs/remotes/origin/master
 git svn rebase
 git push origin --all
