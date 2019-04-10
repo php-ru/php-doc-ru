66280012989127d263b41f60ad5d2d1d76883b8a
Alexey Pyltsyn <lex61rus@gmail.com>
2019-04-10 19:49:56 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,10 +4,10 @@ if [[ "$CIRCLE_BRANCH" == "master" ]]; then
     exit 0
 fi
 
-git clone https://github.com/lex111/php-doc-ru.git doc
+git clone https://github.com/php-ru/php-doc-ru.git doc
 cd doc
 git config svn.authorsfile "../svn-authors.txt"
-git remote set-url origin https://lex111:${GITHUB_TOKEN}@github.com/lex111/php-doc-ru
+git remote set-url origin https://lex111:${GITHUB_TOKEN}@github.com/php-ru/php-doc-ru
 git svn init https://svn.php.net/repository/phpdoc/ru/trunk
 git update-ref refs/remotes/git-svn refs/remotes/origin/master
 git svn rebase
